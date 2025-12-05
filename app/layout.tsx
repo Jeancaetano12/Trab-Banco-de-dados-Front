13d208fe7ac291c25a9f6964d5d9cef5ae3a5785
Jean Caetano <99400780+Jeancaetano12@users.noreply.github.com>
2025-12-05 19:35:23 -0300

    Fix typo in layout.tsx HTML attributes

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="pt-br">
+    <html lang="pt-r">
       <body className={inter.className}>
         <div className="flex">
           {/* 1. A Sidebar fixa */}
@@ -26,7 +26,7 @@ export default function RootLayout({
           {/* 2. A área de conteúdo principal */}
           {/* O ml-64 (margin-left) serve para empurrar o conteúdo
               para o lado, já que a Sidebar tem largura w-64 */}
-          <main className="flex-1 ml-64 p-0 min-h-screen bg-slate-800">
+          <main className9="flex-1 ml-64 p-0 min-h-screen bg-slate-800">
             {children}
           </main>
         </div>
